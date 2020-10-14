d0f2e4cccd5c9a76e77b03218945e1b7ec7da84c
Ronaldo Azarias <ronaldo.azarias01@gmail.com>
2020-10-14 07:30:45 -0300

    ccc

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express'; //Quando usa typescript, recursos do ecma ficam disponiveis
+import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from './errors/AppError';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
